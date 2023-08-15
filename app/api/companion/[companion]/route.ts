f61eac615a32bf44684dcc8b7b473deb506da8fb
Abdullah Bin Anis <rugusakii@gmail.com>
2023-08-15 22:52:07 +0500

    AI Companion

---
+++ app/api/companion/[companion]/route.ts
@@ -0,0 +1,79 @@
+import prismadb from "@/lib/prismasb";
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { companion: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+    if (!params.companion) {
+      return new NextResponse("CompanionId is required", { status: 400 });
+    }
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    // TODO: Check for subscriptions
+
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.companion,
+        userId: user.id
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user?.firstName as string,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("Companion Patch: ", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { companion: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const companion = await prismadb.companion.delete({
+      where: {
+        userId,
+        id: params.companion
+      }
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Menu, Sparkles } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./mode-toggle";
+import MobileSidebar from "./mobile-sidebar";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+export const Navbar = () => {
+  return (
+    <div className="fixed flex w-full z-50 h-16 justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              `hidden md:block text-xl md:text-3xl font-bold text-primary`,
+              font.className
+            )}
+          >
+            companion ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 text-white fill-white ml-2" />
+        </Button>
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
