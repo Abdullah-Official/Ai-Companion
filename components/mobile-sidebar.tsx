a1bc6c8e44bf83e76495ceea50258d93750b31c6
Abdullah Bin Anis <rugusakii@gmail.com>
2023-08-17 00:08:21 +0500

    bug fixed

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,15 +1,17 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Sidebar from "./sidebar";
+import { checkSubscription } from "@/lib/subscription";
 
-const MobileSidebar = () => {
+const MobileSidebar =  async () => {
+  const isPro = await checkSubscription()
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent className="p-0 bg-secondary pt-10 w-32" side="left">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
